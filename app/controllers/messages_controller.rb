07c0a470420f07a2afd21ffed0799318cf1cce09
David29-ra <davidrivasa.12@gmail.com>
2021-12-11 16:13:44 +0000

    Challenge works well but message of balancedo or not not show in screen

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    @messages = sort_by_date(Message.all)
   end
 
   def create
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
     @message.status = its_balanced?(@message.body)
 
     if @message.save
-      flash[:success] = 'Object successfully created'
+      flash[:success] = @message.status.to_s
     else
       flash[:error] = 'Something went wrong'
     end
@@ -18,18 +18,22 @@ class MessagesController < ApplicationController
   private
 
   def its_balanced?(message)
-    message = message.gsub(/(:\(|:\)|[a-z]| |:|\([a-z]*:[a-z]*\))/, "")
-    valid_braces(message) ? "balanceado" : "desbalanceado"
+    message = message.gsub(/(:\(|:\)|[a-z]| |:|\([a-z]*:[a-z]*\))/, '')
+    valid_braces(message) ? 'balanceado' : 'desbalanceado'
   end
 
   def valid_braces(braces)
     stack = []
-    symbols = { "(" => ")" }
+    symbols = { '(' => ')' }
 
     braces.each_char do |brace|
       stack.last && stack.last == symbols.key(brace) ? stack.pop : stack << brace
     end
     stack.empty?
+  end
+
+  def sort_by_date(data)
+    data.sort { |message1, message2| message2[:created_at] <=> message1[:created_at] }
   end
 
   def message_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,52 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+def its_balanced?(message)
+  message = message.gsub(/(:\(|:\)|[a-z]| |:|\([a-z]*:[a-z]*\))/, "")
+  valid_braces(message) ? "balanceado" : "desbalanceado"
+end
+
+def valid_braces(braces)
+  stack = []
+  symbols = { "(" => ")" }
+
+  braces.each_char do |brace|
+    stack.last && stack.last == symbols.key(brace) ? stack.pop : stack << brace
+  end
+  stack.empty?
+end
+
 puts "Start seeding examples"
+examples_h = Message.new({ body: "a (b (c (d) c) b) a :)" })
+examples_h.status = its_balanced?(examples_h.body)
+examples_h.save
+
+examples_g = Message.new({ body: ":((" })
+examples_g.status = its_balanced?(examples_g.body)
+examples_g.save
+
+examples_f = Message.new({ body: "hoy pm: fiesta :):)" })
+examples_f.status = its_balanced?(examples_f.body)
+examples_f.save
+
+examples_e = Message.new({ body: "no voy (:()" })
+examples_e.status = its_balanced?(examples_e.body)
+examples_e.save
+
+examples_d = Message.new({ body: "(:)" })
+examples_d.status = its_balanced?(examples_d.body)
+examples_d.save
+
+examples_c = Message.new({ body: "(()" })
+examples_c.status = its_balanced?(examples_c.body)
+examples_c.save
+
+examples_b = Message.new({ body: "(hola)" })
+examples_b.status = its_balanced?(examples_b.body)
+examples_b.save
+
+examples_a = Message.new({ body: "hola" })
+examples_a.status = its_balanced?(examples_a.body)
+examples_a.save
+
 puts "FInish seeding examples"
